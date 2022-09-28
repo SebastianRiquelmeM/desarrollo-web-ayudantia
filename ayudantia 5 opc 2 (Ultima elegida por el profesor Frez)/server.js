2f7cbe1dbbb061bf54152be6acf4b3197fdb16eb
Sebastian Riquelme <sebarsalbo@gmail.com>
2022-09-28 17:43:56 -0300

    ayudantia 5 opc 2 ultima elegida por el profesor frez

--- ayudantia 5 opc 2 (Ultima elegida por el profesor Frez)/server.js
+++ ayudantia 5 opc 2 (Ultima elegida por el profesor Frez)/server.js
@@ -11,16 +11,22 @@ const app = express();
 
 app.engine("handlebars", engine());
 
-
 app.set("view engine", "handlebars");
 
 //Indico donde estan mis views
 app.set("views", path.resolve(__dirname, "./views"));
 
+// serve all other static files like CSS, images, etc
+app.use(express.static(`${__dirname}/views`));
+
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
 
+app.get("/ticketera", (req, res) => {
+	res.render("ticketera");
+});
 
 app.listen(3000, () => {
 	console.log("express-handlebars example server listening on: 3000");
